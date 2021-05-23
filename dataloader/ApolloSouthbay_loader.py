ed9348fab6bfd67148ac7516e3b7f4ccfe8b2401
Amnon Drory <amnon.drory@gmail.com>
2021-05-23 17:19:28 +0300

    Created LidarFeatureExtractor which can use any of DGR's dataloaders to create the inputs that PointDSC expects, on-the-fly.

---
+++ dataloader/ApolloSouthbay_loader.py
@@ -0,0 +1,305 @@
+# Copyright (c) Chris Choy (chrischoy@ai.stanford.edu) and Wei Dong (weidong@andrew.cmu.edu)
+#
+# Please cite the following papers if you use any part of the code.
+# - Christopher Choy, Wei Dong, Vladlen Koltun, Deep Global Registration, CVPR 2020
+# - Christopher Choy, Jaesik Park, Vladlen Koltun, Fully Convolutional Geometric Features, ICCV 2019
+# - Christopher Choy, JunYoung Gwak, Silvio Savarese, 4D Spatio-Temporal ConvNets: Minkowski Convolutional Neural Networks, CVPR 2019
+import os
+import glob
+import pickle
+import pandas as pd
+
+from dataloader.base_loader import *
+from dataloader.transforms import *
+from util.pointcloud import get_matching_indices, make_open3d_point_cloud
+
+computer_name = os.uname()[1]
+if computer_name in ['ad-2021']:
+  apollo_southbay_dir = '/home/ad/old_drive/data/apollo_subsets/apollo_meta/'
+elif computer_name in ['deep3d']:
+  apollo_southbay_dir = '/data/amnon/datasets/apollo/'  
+else:
+  default_kitti_dir = None
+  default_weights = None
+
+
+
+class Apollo_FCGF_utils():
+
+    @staticmethod
+    def init_session(session_ind):
+        pass
+
+    @staticmethod
+    def is_train_session(session_ind, subset):
+        session_path = Apollo_FCGF_utils.get_session_path(session_ind)
+        if subset == 'disjoint':
+            valid_sequences = ['MathildaAVE','BaylandsToSeafood','ColumbiaPark']   
+            is_ok = False
+            for seq in valid_sequences:
+                is_ok = is_ok or (seq in session_path)
+        else:
+            is_ok = True
+
+        return (("TrainData" in session_path) and is_ok)
+
+    @staticmethod
+    def is_test_session(session_ind, subset):
+        session_path = Apollo_FCGF_utils.get_session_path(session_ind)
+        if subset == 'disjoint':
+            valid_sequences = ['HighWay237','SunnyvaleBigloop','SanJoseDowntown']
+            is_ok = False
+            for seq in valid_sequences:
+                is_ok = is_ok or (seq in session_path)
+        else:
+            is_ok = True
+
+        return (("Test" in session_path) and is_ok)
+
+    def is_validation_session(session_ind, subset):
+        if subset == 'disjoint':
+            return Apollo_FCGF_utils.is_train_session(session_ind, subset)
+        else:
+            return Apollo_FCGF_utils.is_test_session(session_ind, subset)
+
+    @staticmethod
+    def is_phase_session(session_ind, phase, subset):
+        if (phase == 'train'):
+            return Apollo_FCGF_utils.is_train_session(session_ind, subset)
+        elif  (phase == 'val'):
+            return Apollo_FCGF_utils.is_validation_session(session_ind, subset)
+        elif  (phase == 'test'):
+            return Apollo_FCGF_utils.is_test_session(session_ind, subset)            
+        else:
+            assert False, "unknown phase: " + phase
+
+    @staticmethod
+    def dataset_directory():
+        return apollo_southbay_dir
+
+    @staticmethod
+    def get_all_session_paths():
+        session_paths_file = Apollo_FCGF_utils.dataset_directory() + 'session_paths.txt'
+        with open(session_paths_file, "r") as fid:
+            session_paths_relative = fid.read().splitlines()
+
+        session_paths = [Apollo_FCGF_utils.dataset_directory() + p for p in session_paths_relative]
+        return session_paths
+
+    @staticmethod
+    def get_session_path(session_ind):
+        session_paths = Apollo_FCGF_utils.get_all_session_paths()
+        return session_paths[session_ind]
+
+    @staticmethod
+    def num_sessions():
+        session_paths = Apollo_FCGF_utils.get_all_session_paths()
+        return len(session_paths)
+
+    @staticmethod
+    def load_PC(session_ind, index):
+        session_path = Apollo_FCGF_utils.get_session_path(session_ind)
+        filename = session_path + "pcds/%d.pcd" % index
+        if not os.path.isfile(filename):
+            source = filename.replace(apollo_southbay_dir,'amnon@Geoffrey:/mnt3/dataset/apollo/')
+            cmd = 'rsync -avzh %s %s' % (source, filename)
+            print(cmd)
+        assert os.path.isfile(filename), "Error: could not find file " + filename
+        pcd = o3d.io.read_point_cloud(filename)
+        return np.asarray(pcd.points).astype(np.float32)
+
+    @staticmethod
+    def get_pairs(session_ind, subset):
+        ses_path = Apollo_FCGF_utils.get_session_path(session_ind)
+        if subset == '10m':
+            pairs_file = ses_path + 'pairs/FCGF_poses_gt_for_DGR.txt'
+            with open(pairs_file, 'r') as fid:
+                lines = fid.read().splitlines()
+            pairs = []
+            for line in lines:
+                pairs.append([session_ind] + [float(a) for a in line.split()])
+        elif subset in ['balanced', 'disjoint']:
+            pairs_file = ses_path + 'pairs/poses_gt.balanced.DGR_format.txt'
+            raw_ds = pd.read_csv(pairs_file, sep=" ", header=0).values
+            pairs = np.hstack([session_ind*np.ones([raw_ds.shape[0],1]),raw_ds])            
+        return pairs
+
+class ApolloSouthbayPairDataset(PairDataset):
+  AUGMENT = None
+  DATA_FILES = {
+      'train': './dataloader/split/train_kitti.txt',
+      'val': './dataloader/split/val_kitti.txt',
+      'test': './dataloader/split/test_kitti.txt'
+  }
+  TEST_RANDOM_ROTATION = False
+  IS_ODOMETRY = True
+
+  def prep_pairs_list(self):
+      pairs_GT = []
+      for session_ind in range(self.U.num_sessions()):
+          if not self.U.is_phase_session(session_ind, self.phase, self.SUBSET):
+              continue
+          self.U.init_session(session_ind)
+
+          cur_pairs_GT = self.U.get_pairs(session_ind, self.SUBSET)
+
+          pairs_GT.append(cur_pairs_GT)
+
+      pairs_GT = np.vstack(pairs_GT)
+
+      if (self.SUBSET != 'disjoint') and (self.phase in ['val', 'test']):
+          if self.SUBSET == '10m':
+              NUM_VALIDATION_SAMPLES = 210
+          elif self.SUBSET == 'balanced':
+              NUM_VALIDATION_SAMPLES = 300
+          else:
+              assert False, f"Unknown value for self.SUBSET : {self.SUBSET}"
+
+          N = pairs_GT.shape[0]            
+          validation_inds = np.round(np.linspace(0, N-1, NUM_VALIDATION_SAMPLES)).astype(int)
+          validation_mask = np.zeros([N], dtype=bool)
+          test_mask = np.ones([N], dtype=bool)
+          validation_mask[validation_inds] = True
+          test_mask[validation_inds] = False
+          if self.phase == 'val':
+              pairs_GT = pairs_GT[validation_mask,:]
+          elif self.phase == 'test':
+              pairs_GT = pairs_GT[test_mask,:]
+
+      if (self.SUBSET == 'disjoint') and (self.phase in ['val', 'train']):
+          NUM_VALIDATION_SAMPLES = 200
+
+          N = pairs_GT.shape[0]
+          validation_inds = np.round(np.linspace(0, N-1, NUM_VALIDATION_SAMPLES)).astype(int)
+          validation_mask = np.zeros([N], dtype=bool)
+          train_mask = np.ones([N], dtype=bool)
+          validation_mask[validation_inds] = True
+          train_mask[validation_inds] = False
+          if self.phase == 'val':
+              pairs_GT = pairs_GT[validation_mask,:]
+          elif self.phase == 'train':
+              pairs_GT = pairs_GT[train_mask,:]
+      
+      N = pairs_GT.shape[0]
+      if (self.SUBSET == 'disjoint') and (self.phase=='test'):
+          NUM_TEST_SAMPLES = 500
+          if NUM_TEST_SAMPLES < N:
+            keep_inds = np.round(np.linspace(0, N-1, NUM_TEST_SAMPLES)).astype(int)
+            pairs_GT = pairs_GT[keep_inds,:]
+
+      return pairs_GT
+
+  def __init__(self,
+               phase,
+               transform=None,
+               random_rotation=True,
+               random_scale=True,
+               manual_seed=False,
+               config=None,
+               rank=None):
+    # For evaluation, use the odometry dataset training following the 3DFeat eval method    
+    random_rotation = self.TEST_RANDOM_ROTATION    
+    PairDataset.__init__(self, phase, transform, random_rotation, random_scale,
+                         manual_seed, config, rank)
+
+    if self.rank == 0:
+      logging.info(f"Loading the subset {phase}")
+    
+    self.phase = phase
+    self.U = Apollo_FCGF_utils()
+    self.pairs = self.prep_pairs_list()
+
+  def __len__(self):
+    return len(self.pairs)
+
+  def __getitem__(self, idx):
+    pair = self.pairs[idx]
+    drive = int(pair[0])
+    t0, t1 = int(pair[1]), int(pair[2])
+    M2 = pair[3:].reshape([4,4])
+
+    xyz0 = self.U.load_PC(drive, t0)
+    xyz1 = self.U.load_PC(drive, t1)
+
+    if self.random_rotation:
+      T0 = sample_almost_planar_rotation(self.randg)
+      T1 = sample_almost_planar_rotation(self.randg)
+      trans = T1 @ M2 @ np.linalg.inv(T0)
+
+      xyz0 = self.apply_transform(xyz0, T0)
+      xyz1 = self.apply_transform(xyz1, T1)
+    else:
+      trans = M2
+
+    matching_search_voxel_size = self.matching_search_voxel_size
+    if self.random_scale and random.random() < 0.95:
+      scale = self.min_scale + \
+          (self.max_scale - self.min_scale) * random.random()
+      matching_search_voxel_size *= scale
+      xyz0 = scale * xyz0
+      xyz1 = scale * xyz1
+      M2[:3,3] *= scale # this line should appear here, as well as in load_kitti, in FCGF code and anywhere scaling occurs. Its absence is a bug.
+
+    # Voxelization
+    xyz0_th = torch.from_numpy(xyz0)
+    xyz1_th = torch.from_numpy(xyz1)
+
+    _, sel0 = ME.utils.sparse_quantize(xyz0_th / self.voxel_size, return_index=True)
+    _, sel1 = ME.utils.sparse_quantize(xyz1_th / self.voxel_size, return_index=True)
+
+    # Make point clouds using voxelized points
+    pcd0 = make_open3d_point_cloud(xyz0[sel0])
+    pcd1 = make_open3d_point_cloud(xyz1[sel1])
+
+    # Get matches
+    matches = get_matching_indices(pcd0, pcd1, trans, matching_search_voxel_size)
+    if len(matches) < 1000:
+      if phase == 'test':
+        return None
+      else:
+        raise ValueError(f"Insufficient matches in {drive}, {t0}, {t1}")
+
+    # Get features
+    npts0 = len(sel0)
+    npts1 = len(sel1)
+
+    feats_train0, feats_train1 = [], []
+
+    unique_xyz0_th = xyz0_th[sel0]
+    unique_xyz1_th = xyz1_th[sel1]
+
+    feats_train0.append(torch.ones((npts0, 1)))
+    feats_train1.append(torch.ones((npts1, 1)))
+
+    feats0 = torch.cat(feats_train0, 1)
+    feats1 = torch.cat(feats_train1, 1)
+
+    coords0 = torch.floor(unique_xyz0_th / self.voxel_size)
+    coords1 = torch.floor(unique_xyz1_th / self.voxel_size)
+
+    if self.transform:
+      coords0, feats0 = self.transform(coords0, feats0)
+      coords1, feats1 = self.transform(coords1, feats1)
+
+    extra_package = {'drive': drive, 't0': t0, 't1': t1}
+
+    return (unique_xyz0_th.float(),
+            unique_xyz1_th.float(), coords0.int(), coords1.int(), feats0.float(),
+            feats1.float(), matches, trans, extra_package)
+
+class ApolloSouthbayNMPairDataset(ApolloSouthbayPairDataset):
+  SUBSET='10m'
+
+class ApolloSouthbayBalancedPairDataset(ApolloSouthbayPairDataset):
+  SUBSET='balanced'  
+  
+class ApolloSouthbayDisjointPairDataset(ApolloSouthbayPairDataset):
+  SUBSET='disjoint'    
+
+if __name__ == "__main__":
+  with open('config.pickle', 'rb') as fid:
+        config = pickle.load(fid)
+  
+  AS = ApolloSouthbayNMPairDataset('train', rank=0, config=config)
+  r = AS.__getitem__(0)
+  print(r)
--- datasets/LidarFeatureExtractor.py
+++ datasets/LidarFeatureExtractor.py
@@ -2,32 +2,28 @@ import os
 import torch.utils.data as data
 from utils.pointcloud import make_point_cloud, estimate_normal
 from utils.SE3 import *
+from dataloader.kitti_loader import KITTINMPairDataset # AD TODO - this doesn't belong here. The actual dset should be supplied as an argument to __init__
+from easydict import EasyDict as edict # AD TODO - this doesn't belong here.
+from misc.fcgf import ResUNetBN2C as FCGF
+import MinkowskiEngine as ME
 
 from general.paths import kitti_dir, fcgf_weights_file
 
-class KITTIDataset(data.Dataset):
+class LidarFeatureExtractor(data.Dataset):
     def __init__(self,
-                root,
-                split='train',
-                descriptor='fcgf',
+                dataset,
                 in_dim=6,
-                inlier_threshold=0.60,
                 num_node=5000,
                 use_mutual=True,
-                downsample=0.30,
                 augment_axis=0,
                 augment_rotation=1.0,
                 augment_translation=0.01,
                 ):
-        self.root = root
-        self.split = split
-        self.descriptor = descriptor
-        assert descriptor in ['fcgf', 'fpfh']
+
         self.in_dim = in_dim
-        self.inlier_threshold = inlier_threshold
+        self.descriptor = 'fcgf'
         self.num_node = num_node
         self.use_mutual = use_mutual
-        self.downsample = downsample
         self.augment_axis = augment_axis
         self.augment_rotation = augment_rotation
         self.augment_translation = augment_translation
@@ -35,25 +31,47 @@ class KITTIDataset(data.Dataset):
         # containers
         self.ids_list = []
 
-        for filename in os.listdir(f"{self.root}/{descriptor}_{split}/"):
-            self.ids_list.append(os.path.join(f"{self.root}/{descriptor}_{split}/", filename))
+        self.dataset = dataset
+        self.split = self.dataset.phase
+        self.downsample = self.dataset.voxel_size
+        self.inlier_threshold = 2*self.dataset.voxel_size
 
-        # self.ids_list = sorted(self.ids_list, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+        self.model = FCGF(
+            1,
+            32,
+            bn_momentum=0.05,
+            conv1_kernel_size=5,
+            normalize_feature=True
+        ).cuda()
+        checkpoint = torch.load(fcgf_weights_file)
+        self.model.load_state_dict(checkpoint['state_dict'])
+        self.model.eval()               
+
 
     def __getitem__(self, index):
-        # load meta data
-        filename = self.ids_list[index]
-        data = np.load(filename)
-        src_keypts = data['xyz0']
-        tgt_keypts = data['xyz1']
-        src_features = data['features0']
-        tgt_features = data['features1']
+        
+        unique_xyz0_th, unique_xyz1_th, coords0, coords1, feats0, feats1, matches, trans, extra_package = self.dataset.__getitem__(index)
+        coords0, coords1, feats0, feats1 = coords0.cuda(), coords1.cuda(), feats0.cuda(), feats1.cuda()
+
+        # AD TODO: it might be wasteful that we send each cloud separately through the FCGF network, instead of sending an entire batch together.
+        coords0 = ME.utils.batched_coordinates([coords0.float()])
+        stensors0 = ME.SparseTensor(feats0, coordinates=coords0, device=feats0.device)
+        features0 = self.model(stensors0).F        
+
+        coords1 = ME.utils.batched_coordinates([coords1.float()])
+        stensors1 = ME.SparseTensor(feats1, coordinates=coords1, device=feats0.device)
+        features1 = self.model(stensors1).F        
+
+        src_keypts = unique_xyz0_th.detach().cpu().numpy()
+        tgt_keypts = unique_xyz1_th.detach().cpu().numpy()
+        src_features = features0.detach().cpu().numpy()
+        tgt_features = features1.detach().cpu().numpy()
         if self.descriptor == 'fpfh':
             src_features = src_features / (np.linalg.norm(src_features, axis=1, keepdims=True) + 1e-6)
             tgt_features = tgt_features / (np.linalg.norm(tgt_features, axis=1, keepdims=True) + 1e-6)
 
         # compute ground truth transformation
-        orig_trans = data['gt_trans']
+        orig_trans = trans
         
         # AD OBSERVATION - dramatic augmentation of the xyz clouds, AFTER the
         #           FCGF features have already been calculated. This is
@@ -69,7 +87,7 @@ class KITTIDataset(data.Dataset):
         #                 more reasonable to limt rotation to almost-planar
 
         # AD OBSERVATION: this augmentation is also performed on validation set. Therefore, validation outputs are
-        # not deterministic. For test set the augmentation parameters are set to 0, therefore the
+        # not deterministic. For test setm the augmentation parameters are set to 0, therefore the
         # augmentation functions do nothing.
 
         aug_R = rotation_matrix(self.augment_axis, self.augment_rotation)
@@ -154,19 +172,36 @@ class KITTIDataset(data.Dataset):
             labels.astype(np.float32),
 
     def __len__(self):
-        return len(self.ids_list)
+        return len(self.dataset)
 
 if __name__ == "__main__":
-    dset = KITTIDataset(
-                    root=kitti_dir,
-                    split='test',
-                    descriptor='fcgf',
+    phase = 'train'
+    dset = KITTINMPairDataset(
+        phase,
+        transform=None,
+        random_rotation=False,
+        random_scale=False,
+        manual_seed=False,
+        config=edict(
+            {'kitti_dir': os.path.split(kitti_dir)[0], 
+            'icp_cache_path': 'icp',
+            'voxel_size': 0.3, 
+            'positive_pair_search_voxel_size_multiplier': 1.5,
+            }),
+        rank=0)
+
+    ex = LidarFeatureExtractor(
+                    dataset=dset,
                     num_node=5000,
                     use_mutual=False,
                     augment_axis=0,
                     augment_rotation=0,
                     augment_translation=0.00
                     )
-    print(len(dset))
-    for i in range(dset.__len__()):
-        ret_dict = dset[i]
+
+    print(len(ex))
+    for i in range(ex.__len__()):
+        ret_dict = ex.__getitem__(i)
+        for k in ret_dict:
+            print('%f ' % k.flatten()[0], end=None)
+        print('')
